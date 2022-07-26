c9bc4e9aa64f114516c09cd51f1be3d51f24fb41
Eduardo Fernandes <eduardo.eduesf@gmail.com>
2022-07-26 16:14:03 -0300

    Resoluçõa dos 3 Exercícios - a009-String - Eduardo Fernandes

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+// Resolução do Exercício 1
+
+const nomeUsuario = prompt ("Qual o seu nome?")
+const comida1 = prompt ("Em uma lista de 3, qual seria a sua comida preferida?")
+const comida2 = prompt ("Qual seria a comida que fica em segundo lugar?")
+const comida3 = prompt ("Qual a comida que fica em terceiro lugar na sua lista?")
+
+console.log (`Estas são as comidas prefereidas de ${nomeUsuario} \n- ${comida1} \n- ${comida2} \n- ${comida3}`)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,25 @@
+// Resolução do Exercício 3 
+
 //Crie a const para a frase aqui
+
+// a)
+const frase1 = ("Jorge tem uma casa verde e")
+const frase2 = ("com portão azul, com os dizeres:\"BOAS VINDAS,")
+const frase3 = ("mas não deixe o gato sair\"")
+const somaFrase = (`${frase1} ${frase2} ${frase3}`)
+console.log (somaFrase)
+
+// b)
+const trocaFrase1 = frase1.replace("verde","rosa")
+const trocaFrase2 = frase2.replace("azul","laranja")
+const novaFrase = (`${trocaFrase1} ${trocaFrase2} ${frase3}`)
+console.log (novaFrase)
+
+// c)
+console.log ("A nova String inclui a palavra \"verde\"?", novaFrase.includes("verde"))
+console.log ("A nova String inclui a palavra \"laranja\"?", novaFrase.includes("laranja"))
+
+// Extra 
+const fonteFrase3 = (frase3.toUpperCase())
+console.log (fonteFrase3)
+console.log(`${trocaFrase1} ${trocaFrase2} ${fonteFrase3}`)
